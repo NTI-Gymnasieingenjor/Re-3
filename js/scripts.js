3faca4e0f5658d3f2d5b1ecaec8c5962c167e19f
Magnus Lif <magnus.lif@elev.ga.ntig.se>
2021-04-29 13:35:47 +0200

    Added slide show

--- js/scripts.js
+++ js/scripts.js
@@ -3,91 +3,91 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
     */
-   (function($) {
-    "use strict"; // Start of use strict
-  
-      // Smooth scrolling using jQuery easing
-   // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (window.innerWidth < 992) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 100)
-          }, 1000, "easeInOutExpo");
-        }
-        else {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 135)
-          }, 1000, "easeInOutExpo");
-        }
-          return false;
+(function($) {
+  "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+ // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (window.innerWidth < 992) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 100)
+        }, 1000, "easeInOutExpo");
       }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    if (window.innerWidth < 992) {
-      $('body').scrollspy({
-        target: '#mainNav',
-        offset: 101
-      });
+      else {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 135)
+        }, 1000, "easeInOutExpo");
+      }
+        return false;
     }
-    else {
-      $('body').scrollspy({
-        target: '#mainNav',
-        offset: 136
-      });
-    }
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-scrolled");
-      } else {
-        $("#mainNav").removeClass("navbar-scrolled");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-  
-    // Magnific popup calls
-    $('#portfolio').magnificPopup({
-      delegate: 'a',
-      type: 'image',
-      tLoading: 'Loading image #%curr%...',
-      mainClass: 'mfp-img-mobile',
-      gallery: {
-        enabled: true,
-        navigateByImgClick: true,
-        preload: [0, 1]
-      },
-      image: {
-        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-      }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  if (window.innerWidth < 992) {
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 101
     });
-  
-  })(jQuery); // End of use strict
+  }
+  else {
+    $('body').scrollspy({
+      target: '#mainNav',
+      offset: 136
+    });
+  }
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Magnific popup calls
+  $('#portfolio').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1]
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+    }
+  });
+
+})(jQuery); // End of use strict
 
 // Collapsible 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+coll[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight){
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+});
 }
